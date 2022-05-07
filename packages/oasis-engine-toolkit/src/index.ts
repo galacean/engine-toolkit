58d8a9f29e0f5cba6a5ea55a4127a22f6b191fe7
GuoLei1990 <gl3336563@163.com>
2022-05-07 13:37:25 +0800

    feat: add `oasis-engine-toolkit` package

---
+++ packages/oasis-engine-toolkit/src/index.ts
@@ -0,0 +1,9 @@
+export * from "@oasis-engine-toolkit/controls";
+export * from "@oasis-engine-toolkit/framebuffer-picker";
+export * from "@oasis-engine-toolkit/stats";
+
+
+//@ts-ignore
+export const version = `__buildVersion`;
+
+console.log(`oasis engine toolkit version: ${version}`);
