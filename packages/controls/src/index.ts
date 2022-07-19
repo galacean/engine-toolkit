85d99d7186ae858a09eaa9af8db189a6bc05d0f6
cptbtptpbcptdtptp <njktsmshh@163.com>
2022-07-19 10:39:23 +0800

    fix: export controlhandlertype

--- packages/controls/src/index.ts
+++ packages/controls/src/index.ts
@@ -4,6 +4,7 @@ export { OrthoControl } from "./OrthoControl";
 export { ControlKeyboard } from "./inputDevice/ControlKeyboard";
 export { ControlPointer } from "./inputDevice/ControlPointer";
 export { ControlWheel } from "./inputDevice/ControlWheel";
+export { ControlHandlerType } from "./enums/ControlHandlerType";
 
 export type { IControlInput } from "./inputDevice/IControlInput";
 
