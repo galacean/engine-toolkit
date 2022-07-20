3e0b5200b7a3219ba4a6876a38154c65d3ed778e
zhuxudong <callzhuxudong@163.com>
2022-07-20 12:32:54 +0800

    Add `OutlineManager` to show outline of entities (#26)
    * feat: Add `OutlineManager` to show outline of entities

--- packages/oasis-engine-toolkit/src/index.ts
+++ packages/oasis-engine-toolkit/src/index.ts
@@ -2,8 +2,9 @@ export * from "@oasis-engine-toolkit/controls";
 export * from "@oasis-engine-toolkit/framebuffer-picker";
 export * from "@oasis-engine-toolkit/stats";
 export * from "@oasis-engine-toolkit/auxiliary-lines";
-export * from "@oasis-engine-toolkit/skeleton-helper";
+export * from "@oasis-engine-toolkit/skeleton-viewer";
 export * from "@oasis-engine-toolkit/planar-shadow-material";
+export * from "@oasis-engine-toolkit/outline";
 
 //@ts-ignore
 export const version = `__buildVersion`;
---
+++ packages/outline/src/index.ts
@@ -0,0 +1,1 @@
+export { OutlineManager } from "./OutlineManager";
