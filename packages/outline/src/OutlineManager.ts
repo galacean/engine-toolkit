7d9cb5e0fb2caf57ee0f6563930e45038ae7e804
zhuxudong <callzhuxudong@163.com>
2022-08-22 21:23:06 +0800

    refactor: use layer method

--- packages/outline/src/OutlineManager.ts
+++ packages/outline/src/OutlineManager.ts
@@ -6,12 +6,11 @@ import {
   Color,
   dependentComponents,
   Entity,
+  Layer,
   Material,
-  Matrix,
   MeshRenderer,
   PrimitiveMesh,
   RenderTarget,
-  Scene,
   Script,
   Shader,
   Texture2D,
@@ -28,32 +27,37 @@ import vs from "./outline.vs.glsl";
  */
 @dependentComponents(Camera)
 export class OutlineManager extends Script {
+  private static _traverseEntity(entity: Entity, callback: (entity: Entity) => void) {
+    callback(entity);
+    for (let i = entity.children.length - 1; i >= 0; i--) {
+      this._traverseEntity(entity.children[i], callback);
+    }
+  }
   private static _outlineColorProp = Shader.getPropertyByName("u_outlineColor");
   private static _texSizeProp = Shader.getPropertyByName("u_texSize");
 
-  private _outlineScene: Scene;
-  private _material: BaseMaterial;
+  private _outlineMaterial: BaseMaterial;
   private _replaceMaterial: BaseMaterial;
   private _renderTarget: RenderTarget;
-  private _outlineRoot: Entity;
   private _screenEntity: Entity;
   private _size: number = 1;
   private _clearColor: Color = new Color(1, 1, 1, 1);
   private _outlineColor: Color = new Color(0, 0, 0, 1);
   private _replaceColor: Color = new Color(1, 0, 0, 1);
+  private _layer: Layer = Layer.Layer11;
   private _outlineEntities: Entity[] = [];
 
-  private _entityMap: Array<{ entity: Entity; parent: Entity; localMatrix: Matrix }> = [];
-  private _materialMap: Array<{ renderer: MeshRenderer; material: Material }> = [];
   private _renderers: MeshRenderer[] = [];
+  private _materialMap: Array<{ renderer: MeshRenderer; material: Material }> = [];
+  private _layerMap: Array<{ entity: Entity; layer: Layer }> = [];
 
   /** outline color. */
   get color(): Color {
-    return this._material.shaderData.getColor(OutlineManager._outlineColorProp);
+    return this._outlineMaterial.shaderData.getColor(OutlineManager._outlineColorProp);
   }
 
   set color(value: Color) {
-    const color = this._material.shaderData.getColor(OutlineManager._outlineColorProp);
+    const color = this._outlineMaterial.shaderData.getColor(OutlineManager._outlineColorProp);
     if (value !== color) {
       color.copyFrom(value);
     }
@@ -78,8 +82,8 @@ export class OutlineManager extends Script {
     const renderColorTexture = new Texture2D(this.engine, offWidth, offHeight);
     const renderTarget = new RenderTarget(this.engine, offWidth, offHeight, renderColorTexture);
 
-    this._material.shaderData.setTexture("u_texture", renderColorTexture);
-    this._material.shaderData.setVector2(OutlineManager._texSizeProp, new Vector2(1 / offWidth, 1 / offHeight));
+    this._outlineMaterial.shaderData.setTexture("u_texture", renderColorTexture);
+    this._outlineMaterial.shaderData.setVector2(OutlineManager._texSizeProp, new Vector2(1 / offWidth, 1 / offHeight));
     renderColorTexture.wrapModeU = renderColorTexture.wrapModeV = TextureWrapMode.Clamp;
     this._renderTarget = renderTarget;
   }
@@ -87,23 +91,20 @@ export class OutlineManager extends Script {
   constructor(entity: Entity) {
     super(entity);
     const engine = this.engine;
-    const outlineScene = new Scene(engine);
-    const material = new BaseMaterial(engine, Shader.find("outline-postprocess-shader"));
+    const outlineMaterial = new BaseMaterial(engine, Shader.find("outline-postprocess-shader"));
     const replaceMaterial = new UnlitMaterial(engine);
-    const outlineRoot = outlineScene.createRootEntity();
-    const screenEntity = outlineScene.createRootEntity("screen");
+    const screenEntity = this.entity.createChild("screen");
     const screenRenderer = screenEntity.addComponent(MeshRenderer);
 
     replaceMaterial.baseColor = this._replaceColor;
+    screenEntity.layer = this._layer;
     screenRenderer.mesh = PrimitiveMesh.createPlane(engine, 2, 2);
-    screenRenderer.setMaterial(material);
-    material.isTransparent = true;
-    material.shaderData.setColor(OutlineManager._outlineColorProp, this._outlineColor);
+    screenRenderer.setMaterial(outlineMaterial);
+    outlineMaterial.isTransparent = true;
+    outlineMaterial.shaderData.setColor(OutlineManager._outlineColorProp, this._outlineColor);
 
-    this._outlineScene = outlineScene;
-    this._material = material;
+    this._outlineMaterial = outlineMaterial;
     this._replaceMaterial = replaceMaterial;
-    this._outlineRoot = outlineRoot;
     this._screenEntity = screenEntity;
     this.size = this._size;
   }
@@ -130,85 +131,81 @@ export class OutlineManager extends Script {
     if (!this._outlineEntities.length) return;
     const scene = camera.scene;
     const originalClearFlags = camera.clearFlags;
+    const originalCullingMask = camera.cullingMask;
     const originalEnableFrustumCulling = camera.enableFrustumCulling;
     const originalSolidColor = scene.background.solidColor;
     const originalBackgroundMode = scene.background.mode;
-    const originalScene = this.engine.sceneManager.activeScene;
 
     const renderers = this._renderers;
     const materialMap = this._materialMap;
-    const entityMap = this._entityMap;
+    const layerMap = this._layerMap;
     materialMap.length = 0;
-    entityMap.length = 0;
+    layerMap.length = 0;
 
-    for (let i = 0, length = this._outlineEntities.length; i < length; i++) {
+    for (let i = this._outlineEntities.length - 1; i >= 0; i--) {
       const entity = this._outlineEntities[i];
-      const transform = entity.transform;
-      const originalWorldMatrix = transform.worldMatrix.clone();
-      const originalLocalMatrix = transform.localMatrix.clone();
-      const originalParent = entity.parent;
 
-      this._outlineRoot.addChild(entity);
-      transform.worldMatrix = originalWorldMatrix;
-
-      entityMap.push({
-        entity,
-        parent: originalParent,
-        localMatrix: originalLocalMatrix
-      });
-
+      // replace material
       renderers.length = 0;
       entity.getComponentsIncludeChildren(MeshRenderer, renderers);
-      for (let j = 0; j < renderers.length; j++) {
+      for (let j = renderers.length - 1; j >= 0; j--) {
         const renderer = renderers[j];
         materialMap.push({ renderer, material: renderer.getMaterial() });
         renderer.setMaterial(this._replaceMaterial);
       }
+
+      // replace layer
+      OutlineManager._traverseEntity(entity, (entity) => {
+        layerMap.push({
+          entity,
+          layer: entity.layer
+        });
+        entity.layer = this._layer;
+      });
     }
 
-    this.engine.sceneManager.activeScene = this._outlineScene;
+    // 1. render outline mesh with replace material
     this._screenEntity.isActive = false;
-    this._outlineRoot.isActive = true;
     camera.renderTarget = this._renderTarget;
     scene.background.solidColor = this._clearColor;
     scene.background.mode = BackgroundMode.SolidColor;
+    camera.cullingMask = this._layer;
     camera.render();
 
-    this._outlineRoot.isActive = false;
+    // 2. render screen only
     this._screenEntity.isActive = true;
     camera.renderTarget = null;
     camera.clearFlags = CameraClearFlags.None;
     camera.enableFrustumCulling = false;
+    for (let i = materialMap.length - 1; i >= 0; i--) {
+      const { material, renderer } = materialMap[i];
+      renderer.setMaterial(material);
+    }
+    for (let i = layerMap.length - 1; i >= 0; i--) {
+      const { entity, layer } = layerMap[i];
+      entity.layer = layer;
+    }
     camera.render();
 
+    // 3. restore
     this._screenEntity.isActive = false;
-    this.engine.sceneManager.activeScene = originalScene;
     camera.clearFlags = originalClearFlags;
     camera.enableFrustumCulling = originalEnableFrustumCulling;
+    camera.cullingMask = originalCullingMask;
     scene.background.solidColor = originalSolidColor;
     scene.background.mode = originalBackgroundMode;
-
-    entityMap.forEach(({ entity, parent, localMatrix }) => {
-      if (!parent) {
-        scene.addRootEntity(entity);
-      } else {
-        parent.addChild(entity);
-      }
-      entity.transform.localMatrix = localMatrix;
-    });
-    materialMap.forEach(({ material, renderer }) => {
-      renderer.setMaterial(material);
-    });
   }
 
   /** @internal */
   onDestroy() {
-    this._outlineScene.destroy();
     this._renderTarget.getColorTexture().destroy(true);
     this._renderTarget.destroy();
+    this._screenEntity.destroy();
+
+    this._outlineEntities = null;
     this._renderers = null;
-    this._entityMap = null;
     this._materialMap = null;
+    this._layerMap = null;
   }
 }
 
