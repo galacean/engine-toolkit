29d06b4db955dd8be66cbc7eb2ae3cb06e8d0e55
JujieX <jx2320@columbia.edu>
2022-08-25 11:40:06 +0800

    fix: circle mesh segments increase with radian

--- packages/gizmo/src/CircleMesh.ts
+++ packages/gizmo/src/CircleMesh.ts
@@ -1,83 +1,69 @@
-import { Vector3, Buffer, BufferMesh, Quaternion, Engine, BufferBindFlag, BufferUsage, VertexElement, VertexElementFormat, IndexFormat } from "oasis-engine";
+import { Vector3, Quaternion, Engine, ModelMesh, MeshTopology } from "oasis-engine";
 
 interface CircleMeshOptions {
-    center?: Vector3;
-    normal?: Vector3;
-    segments?: number;
-    startPoint?: Vector3;
-    thetaLength?: number;
+  center?: Vector3;
+  normal?: Vector3;
+  segments?: number;
+  startPoint?: Vector3;
+  thetaLength?: number;
+}
+
+export class CircleMesh {
+  public modelMesh: ModelMesh;
+
+  private center = new Vector3();
+  private normal = new Vector3(0, 0, 1);
+  private startPoint = new Vector3(1.6, 0, 0);
+  private thetaLength: number = Math.PI / 2;
+  private vertices: Array<Vector3> = [];
+  private indices: Array<number> = [];
+  private static _tempVect: Vector3 = new Vector3();
+  private static _tempQuat: Quaternion = new Quaternion();
+  /**
+   * @param options
+   */
+  public constructor(options: CircleMeshOptions, engine: Engine) {
+    this.modelMesh = new ModelMesh(engine);
+    this.update(options);
   }
-  
-  export class CircleMesh extends BufferMesh {
-    private static _tempQuat: Quaternion = new Quaternion();
-    private static _tempVector3: Vector3 = new Vector3();
-  
-    private center = new Vector3();
-    private segments = 48;
-    private normal = new Vector3(0, 0, 1);
-    private startPoint = new Vector3(1.6, 0, 0);
-    private thetaLength: number = Math.PI * 2;
-    private vertexBuffer: Buffer;
-    private indexBuffer: Buffer;
-    private vertices: Float32Array;
-  
-    /**
-     * @param options
-     */
-    public constructor(options: CircleMeshOptions, engine: Engine) {
-      super(engine, "name");
-  
-      const vertexStride = 12;
-      this.vertices = new Float32Array((this.segments + 2) * 3);
-      this.vertexBuffer = new Buffer(
-        engine,
-        BufferBindFlag.VertexBuffer,
-        (this.segments + 2) * vertexStride,
-        BufferUsage.Dynamic
-      );
-  
-      this.setVertexElements([new VertexElement("POSITION", 0, VertexElementFormat.Vector3, 0)]);
-      const segments = this.segments;
-      const indices = new Uint8Array(segments * 3);
-      for (let i = 1; i <= segments; i++) {
-        const start = (i - 1) * 3;
-        indices[start] = i;
-        indices[start + 1] = i + 1;
-        indices[start + 2] = 0;
-      }
-  
-      this.indexBuffer = new Buffer(engine, BufferBindFlag.IndexBuffer, indices);
-  
-      this.setIndexBufferBinding(this.indexBuffer, IndexFormat.UInt8);
-      this.setVertexBufferBinding(this.vertexBuffer, vertexStride);
-      this.addSubMesh(0, indices.length);
-      this.update(options);
+
+  public update(options: CircleMeshOptions) {
+    if (options.center) {
+      this.center = options.center.clone();
     }
-  
-    public update(options: CircleMeshOptions) {
-      if (options.center) {
-        this.center = options.center.clone();
-      }
-      if (options.normal) {
-        this.normal = options.normal.clone();
-      }
-      if (options.startPoint) {
-        this.startPoint = options.startPoint.clone();
-      }
-      this.thetaLength = options.thetaLength ?? this.thetaLength;
-  
-      const vertices = this.vertices;
-      // center point
-      this.center.copyToArray(vertices, 0);
-  
-      const segments = this.segments;
-      for (let s = 0; s <= segments; s++) {
-        const segment = (s / segments) * this.thetaLength;
-        Quaternion.rotationAxisAngle(this.normal, segment, CircleMesh._tempQuat);
-        Vector3.transformByQuat(this.startPoint, CircleMesh._tempQuat, CircleMesh._tempVector3);
-        CircleMesh._tempVector3.copyToArray(vertices, 3 * s + 3);
-      }
-  
-      this.vertexBuffer.setData(vertices);
+    if (options.normal) {
+      this.normal = options.normal.clone();
     }
+    if (options.startPoint) {
+      this.startPoint = options.startPoint.clone();
+    }
+    this.thetaLength = options.thetaLength ?? this.thetaLength;
+    // 16 segments for every 180 degree
+    const newSegments = Math.abs(Math.ceil((16 * this.thetaLength) / Math.PI));
+    const segments = Math.max(6, newSegments);
+
+    this.indices = [];
+    this.vertices = [];
+
+    // indices
+    for (let i = 1; i <= segments; i++) {
+      const start = (i - 1) * 3;
+      this.indices[start] = i;
+      this.indices[start + 1] = i + 1;
+      this.indices[start + 2] = 0;
+    }
+    // vertices
+    this.vertices.push(this.center);
+    for (let s = 0; s <= segments; s++) {
+      const segment = (s / segments) * this.thetaLength;
+      Quaternion.rotationAxisAngle(this.normal, segment, CircleMesh._tempQuat);
+      Vector3.transformByQuat(this.startPoint, CircleMesh._tempQuat, CircleMesh._tempVect);
+      this.vertices[s + 1] = CircleMesh._tempVect.clone();
+    }
+    this.modelMesh.setPositions(this.vertices);
+    this.modelMesh.setIndices(Uint16Array.from(this.indices));
+    this.modelMesh.clearSubMesh();
+    this.modelMesh.addSubMesh(0, this.indices.length, MeshTopology.Triangles);
+    this.modelMesh.uploadData(false);
   }
+}
