548970b9d84e866d318fcdab25c5c2d902dbc422
cptbtptpbcptdtptp <njktsmshh@163.com>
2022-09-28 17:51:18 +0800

    feat: update freecontrol

--- packages/controls/src/FreeControl.ts
+++ packages/controls/src/FreeControl.ts
@@ -1,15 +1,16 @@
-import { Entity, InputManager, Keys, MathUtil, Script, Transform, Vector3 } from "oasis-engine";
+import { Entity, InputManager, MathUtil, Script, Transform, Vector3 } from "oasis-engine";
+import { ControlHandlerType } from "./enums/ControlHandlerType";
+import { ControlFreeKeyboard } from "./inputDevice/ControlFreeKeyboard";
+import { ControlFreePointer } from "./inputDevice/ControlFreePointer";
+import { IControlInput } from "./inputDevice/IControlInput";
 import { Spherical } from "./Spherical";
-
-// Prevent universal lock.
-const ESP = MathUtil.zeroTolerance;
 
 /**
  * The camera's roaming controller, can move up and down, left and right, and rotate the viewing angle.
  */
 export class FreeControl extends Script {
-  private _input: InputManager;
-  private _cameraTransform: Transform;
+  input: InputManager;
+  inputDevices: IControlInput[] = [ControlFreeKeyboard, ControlFreePointer];
 
   /** Movement distance per second, the unit is the unit before MVP conversion. */
   movementSpeed: number = 1.0;
@@ -20,67 +21,68 @@ export class FreeControl extends Script {
   /** Simulated ground height. */
   floorY: number = 0;
 
-  private _theta: number;
-  private _phi: number;
+  private _cameraTransform: Transform;
   private _spherical: Spherical = new Spherical();
   private _tempVec: Vector3 = new Vector3();
 
   constructor(entity: Entity) {
     super(entity);
-    this._input = this.engine.inputManager;
+    this.input = this.engine.inputManager;
     const transform = (this._cameraTransform = entity.transform);
     /** Init spherical. */
     const { _tempVec: tempVec, _spherical: spherical } = this;
-    Vector3.transformByQuat(tempVec, transform.rotationQuaternion, tempVec);
+    Vector3.transformByQuat(tempVec.set(0, 0, -1), transform.rotationQuaternion, tempVec);
     spherical.setFromVec3(tempVec);
-    this._theta = spherical.theta;
-    this._phi = spherical.phi;
   }
 
-  onUpdate(delta: number): void {
+  onUpdate(deltaTime: number): void {
     if (this.enabled === false) return;
-    // 键盘位移
-    const { _input: input, _cameraTransform: transform } = this;
-    const actualMoveSpeed = (delta / 1000) * this.movementSpeed;
-    transform.translate(new Vector3(0, 0, -1));
-    const vec = new Vector3();
-    if (input.isKeyHeldDown(Keys.ArrowLeft)) {
-      vec.add(new Vector3(-1, 0, 0));
-    }
-    if (input.isKeyHeldDown(Keys.ArrowRight)) {
-      vec.add(new Vector3(1, 0, 0));
-    }
-    if (input.isKeyHeldDown(Keys.ArrowUp)) {
-      vec.add(new Vector3(0, 0, -1));
-    }
-    if (input.isKeyHeldDown(Keys.ArrowDown)) {
-      vec.add(new Vector3(0, 0, 1));
-    }
-    vec.normalize().scale(actualMoveSpeed);
-    transform.translate(vec, true);
-    if (this.floorMock) {
-      const position = transform.position;
-      if (position.y !== this.floorY) {
-        transform.setPosition(position.x, this.floorY, position.z);
+    let curHandlerType = ControlHandlerType.None;
+    const { _tempVec: delta } = this;
+    const { inputDevices, input } = this;
+    for (let i = inputDevices.length - 1; i >= 0; i--) {
+      const handler = inputDevices[i];
+      const handlerType = handler.onUpdateHandler(input);
+      if (handlerType) {
+        curHandlerType |= handlerType;
+        handler.onUpdateDelta(this, delta);
+        switch (handlerType) {
+          case ControlHandlerType.ROTATE:
+            this._rotate(delta);
+            break;
+          case ControlHandlerType.PAN:
+            this._pan(delta, deltaTime);
+            break;
+          default:
+            break;
+        }
       }
     }
-
-    // 鼠标旋转
-    const moveDelta = input.pointerMovingDelta;
-    if (moveDelta && moveDelta.x !== 0 && moveDelta.y !== 0) {
-      const canvas = this.engine.canvas;
-      this._rotate((-moveDelta.x * 180) / canvas.width, (moveDelta.y * 180) / canvas.height);
+    if (this.floorMock) {
+      const position = this._cameraTransform.position;
+      if (position.y !== this.floorY) {
+        this._cameraTransform.setPosition(position.x, this.floorY, position.z);
+      }
     }
   }
 
-  private _rotate(alpha: number = 0, beta: number = 0): void {
-    this._theta += MathUtil.degreeToRadian(alpha);
-    this._phi += MathUtil.degreeToRadian(beta);
-    this._phi = MathUtil.clamp(this._phi, ESP, Math.PI - ESP);
-    this._spherical.theta = this._theta;
-    this._spherical.phi = this._phi;
-    this._spherical.setToVec3(this._tempVec);
-    Vector3.add(this._cameraTransform.position, this._tempVec, this._tempVec);
-    this._cameraTransform.lookAt(this._tempVec, new Vector3(0, 1, 0));
+  private _pan(moveDelta: Vector3, delta: number): void {
+    const actualMoveSpeed = (delta / 1000) * this.movementSpeed;
+    moveDelta.normalize().scale(actualMoveSpeed);
+    this._cameraTransform.translate(moveDelta, true);
+  }
+
+  private _rotate(moveDelta: Vector3): void {
+    if (moveDelta.x !== 0 || moveDelta.y !== 0) {
+      const canvas = this.engine.canvas;
+      const deltaAlpha = (-moveDelta.x * 180) / canvas.width;
+      const deltaPhi = (moveDelta.y * 180) / canvas.height;
+      this._spherical.theta += MathUtil.degreeToRadian(deltaAlpha);
+      this._spherical.phi += MathUtil.degreeToRadian(deltaPhi);
+      this._spherical.makeSafe();
+      this._spherical.setToVec3(this._tempVec);
+      Vector3.add(this._cameraTransform.position, this._tempVec, this._tempVec);
+      this._cameraTransform.lookAt(this._tempVec, new Vector3(0, 1, 0));
+    }
   }
 }
