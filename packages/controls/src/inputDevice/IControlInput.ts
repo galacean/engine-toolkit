327fdeed89888d13c354ec5a1356c3e867426bed
AZhan <njktsmshh@163.com>
2022-09-29 15:15:39 +0800

    Refactor `FreeControl` by using InputManager (#77)
    * feat: update freecontrol

--- packages/controls/src/inputDevice/IControlInput.ts
+++ packages/controls/src/inputDevice/IControlInput.ts
@@ -1,9 +1,10 @@
 import { InputManager, Vector3 } from "oasis-engine";
 import { ControlHandlerType } from "../enums/ControlHandlerType";
+import { FreeControl } from "../FreeControl";
 import { OrbitControl } from "../OrbitControl";
 import { OrthoControl } from "../OrthoControl";
 
 export interface IControlInput {
   onUpdateHandler(input: InputManager): ControlHandlerType;
-  onUpdateDelta(control: OrbitControl | OrthoControl, outDelta: Vector3): void;
+  onUpdateDelta(control: OrbitControl | OrthoControl | FreeControl, outDelta: Vector3): void;
 }
