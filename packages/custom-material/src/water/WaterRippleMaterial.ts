f12a325be8bff97a0e3bc30bbaf7444ce3476afc
JujieX <jx2320@columbia.edu>
2022-11-24 21:06:01 +0800

    fix: refactor ripple material

--- packages/custom-material/src/water/WaterRippleMaterial.ts
+++ packages/custom-material/src/water/WaterRippleMaterial.ts
@@ -1,96 +1,70 @@
-import { BaseMaterial, Color, Engine, MathUtil, Shader, Texture2D, Vector2 } from "oasis-engine";
+import { Shader, Texture2D, Engine, BaseMaterial, Vector2, Vector3 } from "oasis-engine";
 
 const vertexSource = `
- uniform mat4 u_MVPMat;
+  attribute vec3 POSITION;
+  attribute vec4 COLOR_0;
+  attribute vec2 TEXCOORD_0;
+  attribute vec2 TEXCOORD_1;
 
- uniform sampler2D _FoamTex;
- uniform vec4 _FoamTex_ST;
- uniform sampler2D _NormalTex;
- uniform vec4 _NormalTex_ST;
+  uniform mat4 u_MVPMat;
+  
+  uniform float u_time;
+  uniform vec2 u_foam_speed; 
+  uniform vec2 u_distorsion_speed; 
 
- attribute vec3 POSITION;
- attribute vec2 TEXCOORD_0;
+  varying vec2 waterTexCoords;
+  varying vec2 normalTexCoords;
+  varying vec4 v_color;
 
- varying vec2 FoamTexCoord;
- varying vec2 NormalTexCoord;
- varying vec2 v_uv;
- varying vec3 v_position;
+  void main() {
+    gl_Position = u_MVPMat * vec4(POSITION, 1.0);
 
- void main() {
-   gl_Position = u_MVPMat  *  vec4( POSITION, 1.0 );
-
-   FoamTexCoord =  gl_MultiTexCoord0.xy * _FoamTex_ST.xy + _FoamTex_ST.zw;
-   NormalTexCoord =  gl_MultiTexCoord1.xy * _NormalTex_ST.xy + _NormalTex_ST.zw;
-
-   v_uv = TEXCOORD_0;
-   v_position = POSITION;
- }
+    waterTexCoords = TEXCOORD_0 + vec2(u_foam_speed.x * sin(u_time),u_foam_speed.y * cos(u_time));
+    normalTexCoords = TEXCOORD_1 + vec2(u_distorsion_speed.x * cos(u_time),u_distorsion_speed.y * sin(u_time));
+   
+    v_color = COLOR_0;
+  }
   `;
 
-const fragSource = `
- varying vec2 v_uv;
- varying vec3 v_position;
- varying vec2 TextureCoord
- varying vec2 FoamTexCoord;
- varying vec2 NormalTexCoord;
+const fragmentSource = `
+  varying vec4 v_color;
+  varying vec2 waterTexCoords;
+  varying vec2 normalTexCoords;
 
- uniform vec4 u_color;
- uniform float u_time;
+  uniform sampler2D u_normalTex;
+  uniform sampler2D u_waterTex;
 
- uniform sampler2D _FoamTex;
- uniform sampler2D _NormalTex;
+  uniform vec3 u_foamColor;
+  uniform vec2 u_foam_param;
+  uniform float u_distorsion_amount;
 
- uniform float u_foam_speed_x;
- uniform float u_foam_speed_y;
- uniform float u_foam_amount;
- uniform float u_foam_smoothness
+  void main() {
+    vec4 normalTex = texture2D(u_normalTex, normalTexCoords) * 2.0 - 1.0;
+    vec4 waterTex = texture2D(u_waterTex, waterTexCoords + (normalTex.rg * u_distorsion_amount));
 
- uniform float u_distorsion_speed_x;
- uniform float u_distorsion_speed_y;
- uniform float u_distorsion_amount;
+    float alphaComp = v_color.r * waterTex.r * u_foam_param.x;
+    float alpha = pow(alphaComp,2.0);
+    alpha = smoothstep(0.5 - u_foam_param.y, 0.5+ u_foam_param.y, alpha);
+		alpha = clamp(alpha,0.0,1.0);
+    
+    gl_FragColor = vec4(u_foamColor.r,u_foamColor.g,u_foamColor.b,alpha);
+  }
+  `;
 
- void main (void) {
-    vec4 color = u_color;
-
-    #ifdef TEXTURE
-    vec2 foamUV = texture2D(_FoamTex,FoamTexCoord)
-  #endif
-
-    #ifdef NORMALTEXTURE
-    vec2 normalUV = texture2D(_NormalTex,NormalTexCoord)
-  #endif
-
-    vec2 mov_foam = foamUV + vec2(u_time * u_foam_speed_x, u_time * u_foam_speed_y)
-    vec2 mov_normal = normalUV + vec2(cos(u_time) * u_distorsion_speed_x,sin(u_time) * u_distorsion_speed_y)
-
-    vec4 normal = texture2D(_NormalTex, mov_normal) * 2.0 - 1.0
-    vec4 foam = texture2D(_FoamTex,mov_foam + normal.xy * u_distorsion_amount )
-
-    float alphaComp = u_color.x * foam.x * u_foam_amount
-    float alpha = pow(alphaComp,2.0)
-    alpha = smoothstep(0.5 - u_foam_smoothness, 0.5 + u_foam_smoothness, alpha)
-    alpha = saturate(alpha)
-
-    gl_FragColor = vec4(u_color.x,u_color.y,u_color.z,alpha);
-}
-`;
-
-Shader.create("ripple", vertexSource, fragSource);
+Shader.create("ripple", vertexSource, fragmentSource);
 
 export class RippleMaterial extends BaseMaterial {
-  private static _color = Shader.getPropertyByName("u_color");
-  private static _foamSpeedX = Shader.getPropertyByName("u_foam_speed_x");
-  private static _foamSpeedY = Shader.getPropertyByName("u_foam_speed_y");
-  private static _distorsionSpeedX = Shader.getPropertyByName("u_distorsion_speed_x");
-  private static _distorsionSpeedY = Shader.getPropertyByName("u_distorsion_speed_y");
+  private static _foamColor = Shader.getPropertyByName("u_foamColor");
+  private static _foamSpeed = Shader.getPropertyByName("u_foam_speed");
+  private static _foamParam = Shader.getPropertyByName("u_foam_param");
+  private static _distorsionSpeed = Shader.getPropertyByName("u_distorsion_speed");
   private static _distorsionAmount = Shader.getPropertyByName("u_distorsion_amount");
-  private static _foamAmount = Shader.getPropertyByName("u_foam_amount");
-  private static _foamSmoothness = Shader.getPropertyByName("u_foam_smoothness");
 
   static _foamTextureProp = Shader.getPropertyByName("_FoamTex");
   static _normalTextureProp = Shader.getPropertyByName("_NormalTex");
+
   /**
-   * foam texture.
+   * Foam Texture Map
    */
   get foamTexture(): Texture2D {
     return <Texture2D>this.shaderData.getTexture(RippleMaterial._foamTextureProp);
@@ -98,15 +72,10 @@ export class RippleMaterial extends BaseMaterial {
 
   set foamTexture(value: Texture2D) {
     this.shaderData.setTexture(RippleMaterial._foamTextureProp, value);
-    if (value) {
-      this.shaderData.enableMacro("TEXTURE");
-    } else {
-      this.shaderData.disableMacro("TEXTURE");
-    }
   }
 
   /**
-   * normal texture.
+   * Normal Texture Map
    */
   get normalTexture(): Texture2D {
     return <Texture2D>this.shaderData.getTexture(RippleMaterial._normalTextureProp);
@@ -114,92 +83,72 @@ export class RippleMaterial extends BaseMaterial {
 
   set normalTexture(value: Texture2D) {
     this.shaderData.setTexture(RippleMaterial._normalTextureProp, value);
-    if (value) {
-      this.shaderData.enableMacro("NORMALTEXTURE");
-    } else {
-      this.shaderData.disableMacro("NORMALTEXTURE");
-    }
   }
 
   /**
-   * foam color
+   * Foam Color
    */
-  get color(): Color {
-    return this.shaderData.getColor(RippleMaterial._color);
+  get foamColor(): Vector3 {
+    return this.shaderData.getVector3(RippleMaterial._foamColor);
   }
-  set color(val: Color) {
-    this.shaderData.setColor(RippleMaterial._color, val);
+  set foamColor(val: Vector3) {
+    this.shaderData.setVector3(RippleMaterial._foamColor, val);
   }
+
   /**
-   * foam speed
+   * Foam speed on x direction and y direction
+   * foam speed y, foam amount, foam smoothness
    */
   get foamSpeed(): Vector2 {
-    const x = this.shaderData.getFloat(RippleMaterial._foamSpeedX);
-    const y = this.shaderData.getFloat(RippleMaterial._foamSpeedY);
-    return new Vector2(x, y);
+    return this.shaderData.getVector2(RippleMaterial._foamSpeed);
   }
   set foamSpeed(val: Vector2) {
-    this.shaderData.setFloat(RippleMaterial._foamSpeedX, val.x);
-    this.shaderData.setFloat(RippleMaterial._foamSpeedY, val.y);
+    this.shaderData.setVector2(RippleMaterial._foamSpeed, val);
   }
 
   /**
-   * foam amount
+   * Foam Param;
+   * x for foam amount
+   * y for foam smoothness, must between 0 ~ 0.5;
    */
-  get foamAmount(): number {
-    return this.shaderData.getFloat(RippleMaterial._foamAmount);
+  get foamParam(): Vector2 {
+    return this.shaderData.getVector2(RippleMaterial._foamSpeed);
+  }
+  set foamParam(val: Vector2) {
+    this.shaderData.setVector2(RippleMaterial._foamSpeed, val);
   }
 
-  set foamAmount(val: number) {
-    this.shaderData.setFloat(RippleMaterial._foamAmount, val);
-  }
   /**
-   * foam smoothness,must bewtween 0 ~ 0.5
+   * Distorsion Speed on x direction and y direction
    */
-  get foamSmoothness(): number {
-    return this.shaderData.getFloat(RippleMaterial._foamSmoothness);
+  get distorsionSpeed(): Vector3 {
+    return this.shaderData.getVector3(RippleMaterial._distorsionSpeed);
   }
 
-  set foamSmoothness(val: number) {
-    this.shaderData.setFloat(RippleMaterial._foamSmoothness, MathUtil.clamp(val, 0, 0.5));
-  }
-  /**
-   * distorsion speed
-   */
-  get distorsionSpeed(): Vector2 {
-    const x = this.shaderData.getFloat(RippleMaterial._foamSpeedX);
-    const y = this.shaderData.getFloat(RippleMaterial._foamSpeedY);
-    return new Vector2(x, y);
+  set distorsionSpeed(val: Vector3) {
+    this.shaderData.setVector3(RippleMaterial._distorsionSpeed, val);
   }
 
-  set distorsionSpeed(val: Vector2) {
-    this.shaderData.setFloat(RippleMaterial._foamSpeedX, val.x);
-    this.shaderData.setFloat(RippleMaterial._foamSpeedY, val.y);
-  }
   /**
-   * distorsion amount, must bewtween -1 ~ 1
+   * Distorsion Amount, must between -1 ~ 1
    */
   get distorsionAmount(): number {
     return this.shaderData.getFloat(RippleMaterial._distorsionAmount);
   }
 
   set distorsionAmount(val: number) {
-    this.shaderData.setFloat(RippleMaterial._distorsionAmount, MathUtil.clamp(val, -1, 1));
+    this.shaderData.setFloat(RippleMaterial._distorsionAmount, val);
   }
 
   constructor(engine: Engine) {
     super(engine, Shader.find("ripple"));
     this.isTransparent = true;
-    this.alphaCutoff = 0.5;
 
     const shaderData = this.shaderData;
-    shaderData.setColor(RippleMaterial._color, new Color(1.0, 1.0, 1.0, 1.0));
-    shaderData.setFloat(RippleMaterial._foamSpeedX, 0.3);
-    shaderData.setFloat(RippleMaterial._foamSpeedY, 0.3);
-    shaderData.setFloat(RippleMaterial._distorsionSpeedX, 1);
-    shaderData.setFloat(RippleMaterial._distorsionSpeedY, 1);
+    shaderData.setVector3(RippleMaterial._foamColor, new Vector3(1.0, 1.0, 1.0));
+    shaderData.setVector2(RippleMaterial._foamSpeed, new Vector2(0.3, 0.3));
+    shaderData.setVector2(RippleMaterial._foamParam, new Vector2(2.0, 0.05));
+    shaderData.setVector2(RippleMaterial._distorsionSpeed, new Vector2(1.0, 1.0));
     shaderData.setFloat(RippleMaterial._distorsionAmount, 0.03);
-    shaderData.setFloat(RippleMaterial._foamAmount, 2.0);
-    shaderData.setFloat(RippleMaterial._foamSmoothness, 0.05);
   }
 }
