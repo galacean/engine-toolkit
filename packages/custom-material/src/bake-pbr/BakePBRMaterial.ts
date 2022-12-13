f0325480d7e12ff42e3430cd54409f41f1cbe35e
yangfengzzz <yangfengzzz@hotmail.com>
2022-12-13 11:12:15 +0800

    fix: ACES adapt

--- packages/custom-material/src/bake-pbr/BakePBRMaterial.ts
+++ packages/custom-material/src/bake-pbr/BakePBRMaterial.ts
@@ -157,11 +157,11 @@ uniform float u_exposure;
 
 // The standard ACES tonemap function
 vec3 toneMapACES(vec3 x) {
-    float a = 2.51f;
-    float b = 0.03f;
-    float c = 2.43f;
-    float d = 0.59f;
-    float e = 0.14f;
+    float a = 2.51;
+    float b = 0.03;
+    float c = 2.43;
+    float d = 0.59;
+    float e = 0.14;
     return saturate((x*(a*x+b))/(x*(c*x+d)+e));
 }
 
