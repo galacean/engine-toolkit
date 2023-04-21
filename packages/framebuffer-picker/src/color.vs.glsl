84831fd9b9c060090609df8b46256597fb474968
GuoLei1990 <gl3336563@163.com>
2023-04-21 23:31:56 +0800

    fix: shader compile bug

--- packages/framebuffer-picker/src/color.vs.glsl
+++ packages/framebuffer-picker/src/color.vs.glsl
@@ -3,8 +3,8 @@
 #include <blendShape_input>
 
 void main() {
-
     #include <begin_position_vert>
+    #include <begin_normal_vert>
     #include <blendShape_vert>
     #include <skinning_vert>
     #include <position_vert>
