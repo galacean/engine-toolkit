e69b88ea55e6ec0cb3918e86e87eb1f5565ff818
大聪明 <601670314@qq.com>
2023-08-12 19:47:48 -0700

    Create PlaneMesh.ts

---
+++ packages/controls/src/box-selection/PlaneMesh.ts
@@ -0,0 +1,33 @@
+import { Engine, ModelMesh, Vector3 } from "@galacean/engine";
+
+export class PlaneMesh {
+  static createPlane(engine: Engine): ModelMesh {
+    const mesh = new ModelMesh(engine);
+    PlaneMesh._initGeometryData(mesh);
+    return mesh;
+  }
+
+  static _initGeometryData(mesh: ModelMesh) {
+    const positions = new Array<Vector3>(6);
+    positions[0] = new Vector3(1, 1, 0);
+    positions[1] = new Vector3(-1, -1, 0);
+    positions[2] = new Vector3(-1, 1, 0);
+    positions[3] = new Vector3(-1, -1, 0);
+    positions[4] = new Vector3(1, 1, 0);
+    positions[5] = new Vector3(1, -1, 0);
+
+    const indices = new Uint8Array(6);
+    indices[0] = 2;
+    indices[1] = 1;
+    indices[2] = 0;
+    indices[3] = 5;
+    indices[4] = 4;
+    indices[5] = 3;
+
+    mesh.setPositions(positions);
+    mesh.setIndices(indices);
+    mesh.uploadData(true);
+    mesh.addSubMesh(0, 6);
+    return mesh;
+  }
+}
