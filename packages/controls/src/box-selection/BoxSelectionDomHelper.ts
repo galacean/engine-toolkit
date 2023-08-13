fc76f259b417135864c46003b509c29d0378f942
大聪明 <601670314@qq.com>
2023-08-12 19:45:48 -0700

    Update and rename BoxSelectionHelper.ts to BoxSelectionDomHelper.ts

--- packages/controls/src/box-selection/BoxSelectionDomHelper.ts
+++ packages/controls/src/box-selection/BoxSelectionDomHelper.ts
@@ -1,10 +1,5 @@
 import { Vector2 } from "@galacean/engine";
-
-export interface IBoxSelectionHelper {
-  onSelectStart(vec2: Vector2): any;
-  onSelecting(vec2: Vector2): any;
-  onSelectEnd(vec2: Vector2): any;
-}
+import { IBoxSelectionHelper } from "./types";
 
 export class BoxSelectionDomHelper implements IBoxSelectionHelper {
   element: HTMLDivElement;
