fd176a28136e069c6bedf1f023c42cdffce581de
大聪明 <601670314@qq.com>
2023-09-10 23:05:48 -0700

    Create interfaces.ts

---
+++ packages/tween/src/interfaces.ts
@@ -0,0 +1,3 @@
+import type { Vector3, Vector2, Vector4, Quaternion, Color, Matrix, Matrix3x3 } from "@galacean/engine";
+
+export type Interpolation = Vector2 | Vector3 | Vector4 | Quaternion | Color | Matrix | Matrix3x3;
