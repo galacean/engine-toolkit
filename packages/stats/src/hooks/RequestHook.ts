75438571fb221c8e7ea05f650033f18be59c9e2a
gz65555 <gz65555@gmail.com>
2023-12-14 15:02:11 +0800

    feat: support network request

--- packages/stats/src/hooks/RequestHook.ts
+++ packages/stats/src/hooks/RequestHook.ts
@@ -1,58 +1,63 @@
+
 let requestSize = 0;
 
-let originalSend = XMLHttpRequest.prototype.send;
+function hookRequest() {
 
-const cacheMap = new Map<string, number>();
-function addRequestSize(url: string, size: number) {
-  if (cacheMap.get(url) == undefined) {
-    cacheMap.set(url, size);
-    console.log(`request(${size}): ${url}`);
-    requestSize += size;
+  let originalSend = XMLHttpRequest.prototype.send;
+  
+  const cacheMap = new Map<string, number>();
+  function addRequestSize(url: string, size: number) {
+    if (cacheMap.get(url) == undefined) {
+      cacheMap.set(url, size);
+      console.log(`request(${size}): ${url}`);
+      requestSize += size;
+    }
   }
+  
+  XMLHttpRequest.prototype.send = function (body) {
+    this.addEventListener(
+      "load",
+      function () {
+        let size = 0;
+        if (this.responseType === "" || this.responseType === "text") {
+          size = new Blob([JSON.stringify(this.responseText)]).size;
+        } else if (this.response instanceof Blob) {
+          size = this.response.size;
+        } else if (this.response instanceof ArrayBuffer) {
+          size = this.response.byteLength;
+        } else if (this.responseType === "json") {
+          size = new Blob([JSON.stringify(this.response)]).size;
+        }
+  
+        addRequestSize((this as XMLHttpRequest).responseURL, size);
+      },
+      false
+    );
+  
+    originalSend.call(this, body);
+  
+    var originalImageSrc = Object.getOwnPropertyDescriptor(
+      Image.prototype,
+      "src"
+    ).set;
+  
+    this.originalImageSrc = originalImageSrc;
+  
+    Object.defineProperty(Image.prototype, "src", {
+      set: function (value) {
+        fetch(value).then((response) => {
+          if (response.ok) {
+            response.blob().then((blob) => {
+              addRequestSize((this as XMLHttpRequest).responseURL, blob.size);
+            });
+          }
+        });
+        originalImageSrc.call(this, value);
+      },
+    });
+  };
 }
 
-XMLHttpRequest.prototype.send = function (body) {
-  this.addEventListener(
-    "load",
-    function () {
-      let size = 0;
-      if (this.responseType === "" || this.responseType === "text") {
-        size = new Blob([JSON.stringify(this.responseText)]).size;
-      } else if (this.response instanceof Blob) {
-        size = this.response.size;
-      } else if (this.response instanceof ArrayBuffer) {
-        size = this.response.byteLength;
-      } else if (this.responseType === "json") {
-        size = new Blob([JSON.stringify(this.response)]).size;
-      }
-
-      addRequestSize((this as XMLHttpRequest).responseURL, size);
-    },
-    false
-  );
-
-  originalSend.call(this, body);
-
-  var originalImageSrc = Object.getOwnPropertyDescriptor(
-    Image.prototype,
-    "src"
-  ).set;
-
-  this.originalImageSrc = originalImageSrc;
-
-  Object.defineProperty(Image.prototype, "src", {
-    set: function (value) {
-      fetch(value).then((response) => {
-        if (response.ok) {
-          response.blob().then((blob) => {
-            addRequestSize((this as XMLHttpRequest).responseURL, blob.size);
-          });
-        }
-      });
-      originalImageSrc.call(this, value);
-    },
-  });
-};
 
 export class RequestHook {
   private _originalSend;
@@ -64,6 +69,7 @@ export class RequestHook {
 
   constructor() {
     this._hooked = true;
+    hookRequest();
   }
 
   public reset(): void {
