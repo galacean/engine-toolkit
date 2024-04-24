f7000ef9034d5966ff1444a1bf4ebccb7e2504f9
zhuxudong <callzhuxudong@163.com>
2024-04-24 17:27:06 +0800

    refactor: init input

---
+++ packages/shaderlab/src/shaders/input.glsl
@@ -0,0 +1,33 @@
+struct VertexData{
+
+
+}
+
+struct SurfaceData{
+
+}
+
+struct A2v{
+
+}
+
+struct V2f{
+
+}
+
+struct Temp_a2v{
+	
+}
+
+struct Temp_v2f{
+
+}
+
+void intVertexData(in Temp_a2v a2v, out VertexData vertexData){
+
+}
+
+
+void initSurfaceData(in Temp_v2f v2f, out SurfaceData surfaceData){
+
+}
