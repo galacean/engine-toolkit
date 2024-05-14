d850b7074c59ab793fe19ba5e54beb6a563c1df3
zhuxudong <callzhuxudong@163.com>
2024-05-14 20:38:37 +0800

    enhance: split surface init

--- packages/shaderlab/src/shaders/shadingPBR/materialInputPBR.glsl
+++ packages/shaderlab/src/shaders/shadingPBR/materialInputPBR.glsl
@@ -6,6 +6,7 @@
 struct SurfaceData{
 	vec3  position;
     vec3  normal;
+    mat3  tbn;
     vec3  viewDir;
     float dotNV;
     
@@ -127,15 +128,19 @@ float getAARoughnessFactor(vec3 normal) {
 #endif
 
 
-void initGeometry(Temp_Varyings v, inout SurfaceData surfaceData, bool isFrontFacing){
+
+void initCommonSurfaceData(Temp_Varyings v, inout SurfaceData surfaceData, bool isFrontFacing){
     surfaceData.position = v.v_pos;
+    
     #ifdef CAMERA_ORTHOGRAPHIC
-        surfaceData.viewDir =  -camera_Forward;
+        surfaceData.viewDir = -camera_Forward;
     #else
-        surfaceData.viewDir =  normalize(camera_Position - v.v_pos);
+        surfaceData.viewDir = normalize(camera_Position - v.v_pos);
     #endif
+
     #if defined(MATERIAL_HAS_NORMALTEXTURE) || defined(MATERIAL_HAS_CLEAR_COAT_NORMAL_TEXTURE) || defined(MATERIAL_ENABLE_ANISOTROPY)
         mat3 tbn = getTBN(v, isFrontFacing);
+        surfaceData.tbn = tbn;
     #endif
 
     #ifdef MATERIAL_HAS_NORMALTEXTURE
@@ -146,16 +151,109 @@ void initGeometry(Temp_Varyings v, inout SurfaceData surfaceData, bool isFrontFa
 
     surfaceData.dotNV = saturate( dot(surfaceData.normal, surfaceData.viewDir) );
 
+    vec4 baseColor = material_BaseColor;
+    float metal = material_Metal;
+    float roughness = material_Roughness;
+    vec3 specularColor = material_PBRSpecularColor;
+    float glossiness = material_Glossiness;
+    float f0 = pow2( (material_IOR - 1.0) / (material_IOR + 1.0) );
 
+    surfaceData.f0 = f0;
+
+    #ifdef MATERIAL_HAS_BASETEXTURE
+        vec4 baseTextureColor = texture2D(material_BaseTexture, v.v_uv);
+        #ifndef ENGINE_IS_COLORSPACE_GAMMA
+            baseTextureColor = gammaToLinear(baseTextureColor);
+        #endif
+        baseColor *= baseTextureColor;
+    #endif
+
+    #ifdef RENDERER_ENABLE_VERTEXCOLOR
+        baseColor *= v.v_color;
+    #endif
+
+
+    #ifdef MATERIAL_IS_ALPHA_CUTOFF
+        if( baseColor.a < material_AlphaCutoff ) {
+            discard;
+        }
+    #endif
+
+    #ifdef MATERIAL_HAS_ROUGHNESS_METALLIC_TEXTURE
+        vec4 metalRoughMapColor = texture2D( material_RoughnessMetallicTexture, v.v_uv );
+        roughness *= metalRoughMapColor.g;
+        metal *= metalRoughMapColor.b;
+    #endif
+
+    #ifdef MATERIAL_HAS_SPECULAR_GLOSSINESS_TEXTURE
+        vec4 specularGlossinessColor = texture2D(material_SpecularGlossinessTexture, v.v_uv );
+        #ifndef ENGINE_IS_COLORSPACE_GAMMA
+            specularGlossinessColor = gammaToLinear(specularGlossinessColor);
+        #endif
+        specularColor *= specularGlossinessColor.rgb;
+        glossiness *= specularGlossinessColor.a;
+    #endif
+
+
+    #ifdef IS_METALLIC_WORKFLOW
+        surfaceData.diffuseColor = baseColor.rgb * ( 1.0 - metal );
+        surfaceData.specularColor = mix( vec3(f0), baseColor.rgb, metal );
+        surfaceData.roughness = roughness;
+    #else
+        float specularStrength = max( max( specularColor.r, specularColor.g ), specularColor.b );
+        surfaceData.diffuseColor = baseColor.rgb * ( 1.0 - specularStrength );
+        surfaceData.specularColor = specularColor;
+        surfaceData.roughness = 1.0 - glossiness;
+    #endif
+
+    surfaceData.roughness = max(surfaceData.roughness, getAARoughnessFactor(surfaceData.normal));
+    
+    #ifdef MATERIAL_IS_TRANSPARENT
+        surfaceData.opacity = baseColor.a;
+    #else
+        surfaceData.opacity = 1.0;
+    #endif
+
+    vec3 emissiveRadiance = material_EmissiveColor;
+    #ifdef MATERIAL_HAS_EMISSIVETEXTURE
+        vec4 emissiveColor = texture2D(material_EmissiveTexture, v.v_uv);
+        #ifndef ENGINE_IS_COLORSPACE_GAMMA
+            emissiveColor = gammaToLinear(emissiveColor);
+        #endif
+    emissiveRadiance *= emissiveColor.rgb;
+    #endif
+    surfaceData.emissive = emissiveRadiance;
+
+}
+
+void initClearCoatSurfaceData(Temp_Varyings v, inout SurfaceData surfaceData, bool isFrontFacing){
     #ifdef MATERIAL_ENABLE_CLEAR_COAT
         #ifdef MATERIAL_HAS_CLEAR_COAT_NORMAL_TEXTURE
-            surfaceData.clearCoatNormal = getNormalByNormalTexture(tbn, material_ClearCoatNormalTexture, material_NormalIntensity, v.v_uv, isFrontFacing);
+            surfaceData.clearCoatNormal = getNormalByNormalTexture(surfaceData.tbn, material_ClearCoatNormalTexture, material_NormalIntensity, v.v_uv, isFrontFacing);
         #else
             surfaceData.clearCoatNormal = getNormal(v, isFrontFacing);
         #endif
         surfaceData.clearCoatDotNV = saturate( dot(surfaceData.clearCoatNormal, surfaceData.viewDir) );
+
+        surfaceData.clearCoat = material_ClearCoat;
+        surfaceData.clearCoatRoughness = material_ClearCoatRoughness;
+
+        #ifdef MATERIAL_HAS_CLEAR_COAT_TEXTURE
+            surfaceData.clearCoat *= (texture2D( material_ClearCoatTexture, v.v_uv )).r;
+        #endif
+
+        #ifdef MATERIAL_HAS_CLEAR_COAT_ROUGHNESS_TEXTURE
+            surfaceData.clearCoatRoughness *= (texture2D( material_ClearCoatRoughnessTexture, v.v_uv )).g;
+        #endif
+
+        surfaceData.clearCoat = saturate( surfaceData.clearCoat );
+        surfaceData.clearCoatRoughness = max(surfaceData.clearCoatRoughness, getAARoughnessFactor(surfaceData.clearCoatNormal));
+
     #endif
 
+}
+
+void initAnisotropySurfaceData(Temp_Varyings v, inout SurfaceData surfaceData){
     #ifdef MATERIAL_ENABLE_ANISOTROPY
         float anisotropy = material_AnisotropyInfo.z;
         vec3 anisotropicDirection = vec3(material_AnisotropyInfo.xy, 0.0);
@@ -166,106 +264,19 @@ void initGeometry(Temp_Varyings v, inout SurfaceData surfaceData, bool isFrontFa
         #endif
 
         surfaceData.anisotropy = anisotropy;
-        surfaceData.anisotropicT = normalize(tbn * anisotropicDirection);
+        surfaceData.anisotropicT = normalize(surfaceData.tbn * anisotropicDirection);
         surfaceData.anisotropicB = normalize(cross(surfaceData.normal, surfaceData.anisotropicT));
         surfaceData.anisotropicN = getAnisotropicBentNormal(surfaceData);
 
     #endif
-}
-
-void initMaterial(Temp_Varyings v, inout SurfaceData surfaceData){
-        vec4 baseColor = material_BaseColor;
-        float metal = material_Metal;
-        float roughness = material_Roughness;
-        vec3 specularColor = material_PBRSpecularColor;
-        float glossiness = material_Glossiness;
-        float alphaCutoff = material_AlphaCutoff;
-        float f0 = pow2( (material_IOR - 1.0) / (material_IOR + 1.0) );
-
-        surfaceData.f0 = f0;
-
-        #ifdef MATERIAL_HAS_BASETEXTURE
-            vec4 baseTextureColor = texture2D(material_BaseTexture, v.v_uv);
-            #ifndef ENGINE_IS_COLORSPACE_GAMMA
-                baseTextureColor = gammaToLinear(baseTextureColor);
-            #endif
-            baseColor *= baseTextureColor;
-        #endif
-
-        #ifdef RENDERER_ENABLE_VERTEXCOLOR
-            baseColor *= v.v_color;
-        #endif
-
-
-        #ifdef MATERIAL_IS_ALPHA_CUTOFF
-            if( baseColor.a < alphaCutoff ) {
-                discard;
-            }
-        #endif
-
-        #ifdef MATERIAL_HAS_ROUGHNESS_METALLIC_TEXTURE
-            vec4 metalRoughMapColor = texture2D( material_RoughnessMetallicTexture, v.v_uv );
-            roughness *= metalRoughMapColor.g;
-            metal *= metalRoughMapColor.b;
-        #endif
-
-        #ifdef MATERIAL_HAS_SPECULAR_GLOSSINESS_TEXTURE
-            vec4 specularGlossinessColor = texture2D(material_SpecularGlossinessTexture, v.v_uv );
-            #ifndef ENGINE_IS_COLORSPACE_GAMMA
-                specularGlossinessColor = gammaToLinear(specularGlossinessColor);
-            #endif
-            specularColor *= specularGlossinessColor.rgb;
-            glossiness *= specularGlossinessColor.a;
-        #endif
-
-
-        #ifdef IS_METALLIC_WORKFLOW
-            surfaceData.diffuseColor = baseColor.rgb * ( 1.0 - metal );
-            surfaceData.specularColor = mix( vec3(f0), baseColor.rgb, metal );
-            surfaceData.roughness = roughness;
-        #else
-            float specularStrength = max( max( specularColor.r, specularColor.g ), specularColor.b );
-            surfaceData.diffuseColor = baseColor.rgb * ( 1.0 - specularStrength );
-            surfaceData.specularColor = specularColor;
-            surfaceData.roughness = 1.0 - glossiness;
-        #endif
-
-        surfaceData.roughness = max(surfaceData.roughness, getAARoughnessFactor(surfaceData.normal));
-
-        #ifdef MATERIAL_ENABLE_CLEAR_COAT
-            surfaceData.clearCoat = material_ClearCoat;
-            surfaceData.clearCoatRoughness = material_ClearCoatRoughness;
-            #ifdef MATERIAL_HAS_CLEAR_COAT_TEXTURE
-                surfaceData.clearCoat *= (texture2D( material_ClearCoatTexture, v.v_uv )).r;
-            #endif
-            #ifdef MATERIAL_HAS_CLEAR_COAT_ROUGHNESS_TEXTURE
-                surfaceData.clearCoatRoughness *= (texture2D( material_ClearCoatRoughnessTexture, v.v_uv )).g;
-            #endif
-            surfaceData.clearCoat = saturate( surfaceData.clearCoat );
-            surfaceData.clearCoatRoughness = max(surfaceData.clearCoatRoughness, getAARoughnessFactor(surfaceData.clearCoatNormal));
-        #endif
-
-        #ifdef MATERIAL_IS_TRANSPARENT
-            surfaceData.opacity = baseColor.a;
-        #else
-            surfaceData.opacity = 1.0;
-        #endif
-
-        vec3 emissiveRadiance = material_EmissiveColor;
-        #ifdef MATERIAL_HAS_EMISSIVETEXTURE
-            vec4 emissiveColor = texture2D(material_EmissiveTexture, v.v_uv);
-            #ifndef ENGINE_IS_COLORSPACE_GAMMA
-                emissiveColor = gammaToLinear(emissiveColor);
-            #endif
-        emissiveRadiance *= emissiveColor.rgb;
-        #endif
-        surfaceData.emissive = emissiveRadiance;
 
 }
+
 
 void initSurfaceData(Temp_Varyings v, out SurfaceData surfaceData, bool isFrontFacing){
-    initGeometry(v, surfaceData, isFrontFacing);
-    initMaterial(v, surfaceData);
+    initCommonSurfaceData(v, surfaceData, isFrontFacing);
+    initClearCoatSurfaceData(v, surfaceData, isFrontFacing);
+    initAnisotropySurfaceData(v, surfaceData);
 }
 
 
