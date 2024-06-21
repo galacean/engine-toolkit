e15d901056d68789a25dfd30297318ed1216a676
hhhhkrx <zengyan0605@163.com>
2024-06-21 14:39:20 +0800

    refactor：Modify the shader based on the comments

--- packages/custom-material/src/digtal-humen/hair/HairLightDirect.glsl
+++ packages/custom-material/src/digtal-humen/hair/HairLightDirect.glsl
@@ -14,7 +14,7 @@ void diffuseLobe(BRDFData brdfData, vec3 irradiance, float attenuation, inout ve
 void specularLobe(BRDFData brdfData, vec3 incidentDirection, vec3 irradiance, float attenuation, inout vec3 Fs){
 
     vec3 worldtangentDir = brdfData.tangent;
-    vec3 worldBitangentDir = brdfData.bitangent;
+    vec3 worldbitangentDir = brdfData.bitangent;
 
    #ifdef MATERIAL_HAS_HAIRANISOTROPY_TEXTURE
     float shift = (texture2D(material_HairAnisotropyTexture, v.v_uv)).r - 0.5;
@@ -22,8 +22,8 @@ void specularLobe(BRDFData brdfData, vec3 incidentDirection, vec3 irradiance, fl
     float shift = 1.0;
    #endif
   
-    vec3 shiftTangent1 = shiftTangent(worldBitangentDir, brdfData.normal, shift + material_HairFirstOffest);
-    vec3 shiftTangent2 = shiftTangent(worldBitangentDir, brdfData.normal, shift + material_HairSecondOffest);
+    vec3 shiftTangent1 = shiftTangent(worldbitangentDir, brdfData.normal, shift + material_HairFirstOffest);
+    vec3 shiftTangent2 = shiftTangent(worldbitangentDir, brdfData.normal, shift + material_HairSecondOffest);
  
     vec3 firstcol = material_HairFirstColor.rgb;
     vec3 firstSpecular = firstcol * anisotropySpecular(brdfData, incidentDirection, material_HairFirstWidth*15.0, material_HairsFirststrength, shiftTangent1);
