a4d94277dfc092b418f74d96a06554233cbbc8b2
hhhhkrx <zengyan0605@163.com>
2024-07-22 22:49:50 +0800

    refactor: fix eye bug

--- packages/custom-material/src/advanced-shader/eye/EyeForwardPass.glsl
+++ packages/custom-material/src/advanced-shader/eye/EyeForwardPass.glsl
@@ -75,13 +75,14 @@ void PBRFragment(Varyings varyings) {
 
   #ifdef RENDERER_HAS_TANGENT
    mat3 tbn = mat3(surfaceData.tangent, surfaceData.bitangent, surfaceData.normal);
+  #else
+   mat3 tbn = getTBNByDerivatives(aoUV, normal, varyings.positionWS, gl_FrontFacing);
   #endif
 
   // Modify surfaceData by eye algorithm
-  surfaceData.albedoColor = calculateEyeColor(varyings.uv, tbn);
+  surfaceData.albedoColor = calculateEyeColor(varyings.uv, tbn, surfaceData);
   surfaceData.normal =calculateEyeNormal(varyings.uv, tbn, gl_FrontFacing);
   surfaceData.f0 = 0.04; 
-
 
   // Can modify surfaceData here
   initBRDFData(surfaceData, brdfData);
@@ -107,7 +108,6 @@ void PBRFragment(Varyings varyings) {
 
   // Emissive
   color.rgb += surfaceData.emissiveColor;
-
 
   #if SCENE_FOG_MODE != 0
       color = fog(color, varyings.positionVS);
