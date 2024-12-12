8650e813176cebbb2ec68d5579a8781a462c0525
hhhhkrx <zengyan0605@163.com>
2024-12-12 16:02:55 +0800

    fix: indirect brdf

--- packages/shaderlab/src/shaders/shadingPBR/BRDF.glsl
+++ packages/shaderlab/src/shaders/shadingPBR/BRDF.glsl
@@ -341,7 +341,36 @@ vec3 BRDF_Diffuse_Lambert(vec3 diffuseColor) {
         vec3 F = sheenColor;
         return  D * V * F;
     }
+
+    // This is a curve-fit approxmation to the "Charlie sheen" BRDF integrated over the hemisphere from
+    // Estevez and Kulla 2017, "Production Friendly Microfacet Sheen BRDF". The analysis can be found
+    // in the Sheen section of https://drive.google.com/file/d/1T0D1VSyR4AllqIJTQAraEIzjlb5h4FKH/view?usp=sharing
+    float iblSheenDFG(SurfaceData surfaceData, float sheenRoughness) {
+    float dotNV = surfaceData.dotNV;
+    float a = sheenRoughness < 0.25 ? -339.2 * sheenRoughness + 161.4 * sheenRoughness - 25.9 : -8.48 * sheenRoughness + 14.3 * sheenRoughness - 9.95;
+    float b = sheenRoughness < 0.25 ? 44.0 * sheenRoughness - 23.7 * sheenRoughness + 3.26 : 1.97 * sheenRoughness - 3.27 * sheenRoughness + 0.72;
+    float DG = exp( a * dotNV + b ) + ( sheenRoughness < 0.25 ? 0.0 : 0.1 * ( sheenRoughness - 0.25 ) );
+    return saturate( DG * RECIPROCAL_PI );
+    }
 #endif
+
+// ------------------------Indirect Specular------------------------
+// ref: https://www.unrealengine.com/blog/physically-based-shading-on-mobile - environmentBRDF for GGX on mobile
+vec3 envBRDFApprox(vec3 specularColor, float roughness, float dotNV ) {
+
+    const vec4 c0 = vec4( - 1, - 0.0275, - 0.572, 0.022 );
+
+    const vec4 c1 = vec4( 1, 0.0425, 1.04, - 0.04 );
+
+    vec4 r = roughness * c0 + c1;
+
+    float a004 = min( r.x * r.x, exp2( - 9.28 * dotNV ) ) * r.x + r.y;
+
+    vec2 AB = vec2( -1.04, 1.04 ) * a004 + r.zw;
+
+    return specularColor * AB.x + AB.y;
+
+}
 
 void initBRDFData(SurfaceData surfaceData, out BRDFData brdfData){
     vec3 albedoColor = surfaceData.albedoColor;
--- packages/shaderlab/src/shaders/shadingPBR/LightIndirectFunctions.glsl
+++ packages/shaderlab/src/shaders/shadingPBR/LightIndirectFunctions.glsl
@@ -1,24 +1,6 @@
 #ifndef LIGHT_INDIRECT_FUNCTIONS_INCLUDED
 #define LIGHT_INDIRECT_FUNCTIONS_INCLUDED
 #include "Light.glsl"
-// ------------------------Specular------------------------
-// ref: https://www.unrealengine.com/blog/physically-based-shading-on-mobile - environmentBRDF for GGX on mobile
-vec3 envBRDFApprox(vec3 specularColor, float roughness, float dotNV ) {
-
-    const vec4 c0 = vec4( - 1, - 0.0275, - 0.572, 0.022 );
-
-    const vec4 c1 = vec4( 1, 0.0425, 1.04, - 0.04 );
-
-    vec4 r = roughness * c0 + c1;
-
-    float a004 = min( r.x * r.x, exp2( - 9.28 * dotNV ) ) * r.x + r.y;
-
-    vec2 AB = vec2( -1.04, 1.04 ) * a004 + r.zw;
-
-    return specularColor * AB.x + AB.y;
-
-}
-
 
 vec3 getReflectedVector(SurfaceData surfaceData, vec3 n) {
     #ifdef MATERIAL_ENABLE_ANISOTROPY
@@ -66,17 +48,4 @@ vec3 getLightProbeRadiance(SurfaceData surfaceData, vec3 normal, float roughness
 
     #endif
 }
-
-// This is a curve-fit approxmation to the "Charlie sheen" BRDF integrated over the hemisphere from
-// Estevez and Kulla 2017, "Production Friendly Microfacet Sheen BRDF". The analysis can be found
-// in the Sheen section of https://drive.google.com/file/d/1T0D1VSyR4AllqIJTQAraEIzjlb5h4FKH/view?usp=sharing
-#ifdef MATERIAL_ENABLE_SHEEN
-    float iblSheenDFG(SurfaceData surfaceData, float sheenRoughness) {
-        float dotNV = surfaceData.dotNV;
-        float a = sheenRoughness < 0.25 ? -339.2 * sheenRoughness + 161.4 * sheenRoughness - 25.9 : -8.48 * sheenRoughness + 14.3 * sheenRoughness - 9.95;
-        float b = sheenRoughness < 0.25 ? 44.0 * sheenRoughness - 23.7 * sheenRoughness + 3.26 : 1.97 * sheenRoughness - 3.27 * sheenRoughness + 0.72;
-        float DG = exp( a * dotNV + b ) + ( sheenRoughness < 0.25 ? 0.0 : 0.1 * ( sheenRoughness - 0.25 ) );
-        return saturate( DG * RECIPROCAL_PI );
-    }
-#endif
 #endif
