7b8f3d0c8494dde47341f91a629a8ccd135bc03e
hhhhkrx <zengyan0605@163.com>
2024-12-13 01:02:31 +0800

    fix: sheen

--- packages/shaderlab/src/shaders/shadingPBR/BRDF.glsl
+++ packages/shaderlab/src/shaders/shadingPBR/BRDF.glsl
@@ -345,8 +345,8 @@ vec3 BRDF_Diffuse_Lambert(vec3 diffuseColor) {
     }
 
     float prefilteredDFG(float NoV, float perceptualRoughness) {
-    return textureLod( material_SheenLUT, vec2(NoV, perceptualRoughness), 0.0).b;
-}
+        return textureLod(material_SheenLUT, vec2(NoV, perceptualRoughness), 0.0).b;
+    }
 #endif
 
 // ------------------------Indirect Specular------------------------
@@ -398,8 +398,7 @@ void initBRDFData(SurfaceData surfaceData, out BRDFData brdfData){
 
     #ifdef MATERIAL_ENABLE_SHEEN
         brdfData.sheenRoughness = max(MIN_PERCEPTUAL_ROUGHNESS, min(surfaceData.sheenRoughness + getAARoughnessFactor(surfaceData.normal), 1.0));
-        float perceptualRoughness = brdfData.sheenRoughness * brdfData.sheenRoughness;
-        brdfData.approxIBLSheenDG = prefilteredDFG(surfaceData.dotNV, perceptualRoughness);
+        brdfData.approxIBLSheenDG = prefilteredDFG(surfaceData.dotNV, brdfData.sheenRoughness);
         brdfData.sheenScaling = 1.0 - brdfData.approxIBLSheenDG * max(max(surfaceData.sheenColor.r, surfaceData.sheenColor.g), surfaceData.sheenColor.b);
     #endif
 }
