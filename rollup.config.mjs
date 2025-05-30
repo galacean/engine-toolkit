555ff79502fba4c8b420a0ae65f7108b589c3a2c
Kbscript <koubodehao@gmail.com>
2025-05-30 15:25:12 +0800

    chore: replace release action with internal package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -124,7 +124,7 @@ function makeRollupConfig(pkg) {
     configs.push({
       input: path.join(pkg.location, "src", "index.ts"),
       output: {
-        file: path.join(pkg.location, "dist", "umd", "browser.js"),
+        file: path.join(pkg.location, "dist", "browser.js"),
         format: "umd",
         name: umdConfig.name,
         globals: globals
@@ -137,7 +137,7 @@ function makeRollupConfig(pkg) {
   configs.push({
     input: path.join(pkg.location, "src", "index.ts"),
     output: {
-      file: path.join(pkg.location, "dist", "miniprogram.js"),
+      file: path.join(pkg.location, "miniprogram.js"),
       sourcemap: false,
       format: "cjs"
     },
